0197861a9a53506290e5bd8114b99ca78bf0e65f
Aleksashka301 <aleksashka301@gmail.com>
2025-04-16 18:48:10 +0500

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
@@ -22,6 +22,7 @@ def get_related_posts_count(tag):
 
 
 def serialize_post_optimized(post):
+    tags = post.tags.all()
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -30,15 +31,15 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
+        'tags': [serialize_tag(tag) for tag in tags],
+        'first_tag_title': tags[0].title if tags else None,
     }
 
 
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts_with_tag,
     }
 
 
@@ -49,22 +50,19 @@ def get_likes_count(Post):
 
 def index(request):
     most_popular_posts = Post.objects.popular()\
-        .select_related('author')[:5]\
+        .select_related('author')[:5] \
+        .prefetch_related(Prefetch('tags', queryset=Tag.objects.annotate(posts_with_tag=Count('posts'))))[:5]\
         .fetch_with_comments_count()   # TODO. Как это посчитать?
 
-    fresh_posts = Post.objects\
-        .prefetch_related('tags', 'likes')\
+    most_fresh_posts = Post.objects\
         .select_related('author')\
-        .annotate(likes_count=Count('likes'))\
+        .prefetch_related('likes', Prefetch('tags', queryset=Tag.objects.annotate(posts_with_tag=Count('posts'))))\
         .order_by('-published_at')[:5]
 
-    most_fresh_posts = list(fresh_posts)[-5:]
     most_popular_tags = Tag.objects.popular()[:5]
 
     context = {
-        'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
-        ],
+        'most_popular_posts': [serialize_post_optimized(post) for post in most_popular_posts],
         'page_posts': [serialize_post_optimized(post) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
